a4c5f683c3e8992e9c01023cb8dba0dac62e26e9
Autoformatter <autoformatter@users.noreply.github.com>
2022-02-09 16:04:43 +0000

    :art: Committing stylua and shfmt changes changes

--- shell/settings/zsh.sh
+++ shell/settings/zsh.sh
@@ -16,12 +16,12 @@ bindkey '^r' history-incremental-search-backward
 
 bindkey '^[[7~' beginning-of-line                               # Home key
 bindkey '^[[H' beginning-of-line                                # Home key
-if [[ "${terminfo[khome]}" != "" ]]; then
+if [[ ${terminfo[khome]} != ""   ]]; then
     bindkey "${terminfo[khome]}" beginning-of-line              # [Home] - Go to beginning of line
 fi
 bindkey '^[[8~' end-of-line                                     # End key
 bindkey '^[[F' end-of-line                                      # End key
-if [[ "${terminfo[kend]}" != "" ]]; then
+if [[ ${terminfo[kend]} != ""   ]]; then
     bindkey "${terminfo[kend]}" end-of-line                     # [End] - Go to end of line
 fi
 bindkey '^[[2~' overwrite-mode                                  # Insert key
