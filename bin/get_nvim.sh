8eba6ca8b320dddd9e85e4c53581253c11396a02
Mike <mickiller.25@gmail.com>
2023-06-21 13:13:27 +0200

    fix: Checkout to master on --dev

--- bin/get_nvim.sh
+++ bin/get_nvim.sh
@@ -622,17 +622,14 @@ fi
 
 status_msg "Cleaning current build"
 make clean
-# make distclean
-
-# # Get latest version
-# status_msg "Pulling latest changes"
-# git checkout master
-# git pull origin master
 
 if [[ $DEV -eq 0 ]]; then
     BRANCH="${BRANCH:-$(git tag | sort -h | tail -1)}"
     status_msg "Checking out to $BRANCH"
     git checkout "$BRANCH"
+else
+    git checkout master
+    # git pull origin master
 fi
 
 status_msg "Building neovim"
